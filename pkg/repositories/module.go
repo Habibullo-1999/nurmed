0a202f5452d07cb9d6b955c5027368f28c07e0bc
Habibullo-1999 <habibullo.boss@gmail.com>
2026-02-26 21:17:05 +0500

    add module purchases

--- pkg/repositories/module.go
+++ pkg/repositories/module.go
@@ -4,12 +4,14 @@ import (
 	"go.uber.org/fx"
 
 	"nurmed/pkg/repositories/auth"
+	"nurmed/pkg/repositories/purchases"
 	"nurmed/pkg/repositories/sales"
 	"nurmed/pkg/repositories/users"
 )
 
 var Module = fx.Options(
 	auth.Module,
+	purchases.Module,
 	sales.Module,
 	users.Module,
 )
